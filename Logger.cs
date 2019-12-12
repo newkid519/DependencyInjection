858d94efbc75ff5aeb0d5c63a3e13c0e6b9a77fe
newkid519 <newkid519@hotmail.com>
2019-12-12 14:02:45 +0800

    The dependency ILogger is injected in constructor.

---
+++ Logger.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+
+namespace DependencyInjection
+{
+	interface ILogger
+	{
+		void WriteLog(string msg);
+	}
+	
+    class Logger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+            Console.WriteLine($"{msg}");
+        }
+    }
+	
+	class AdvancedLogger : ILogger
+	{
+		public void WriteLog(string msg)
+        {
+            Console.WriteLine($"{msg} at {DateTime.Now}");
+        }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			MyApplication app = new MyApplication(new Logger());
+			app.DoSomeAction();
+        }
+    }
+	
+	class MyApplication
+	{
+		ILogger _logger;
+		
+		public MyApplication(ILogger logger)
+		{
+			_logger = logger;
+		}
+		
+		public void DoSomeAction()
+		{
+			_logger.WriteLog("do some action in MyApplication");
+		}
+	}
+}
